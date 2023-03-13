3b4f4d619f567b031521e99443d80fe545d47f4b
STRANGE88 <vnmstranger@gmail.com>
2023-03-13 15:21:33 +0530

    dhfg

---
+++ script.js
@@ -0,0 +1,71 @@
+// Datatypes
+// var y = 20;
+// {
+//     var x=10;
+//     console.log(x);
+// }
+// console.log(x);
+// var sum = 5+10;
+// console.log(sum);
+// var difference = 18-5;
+// console.log(difference);
+// var multiple = 5*5;
+// console.log(multiple);
+// var division = 20/5;
+// console.log(division);
+// document.write("sum ="+sum );
+// document.write("multiple="+multiple);
+// document.write("difference="+difference);
+// document.write("division="+division);
+// var fname="tiya";
+// var lname="stephen";
+// document.write(`fullname is ${frame} ${lname}`);
+// var student={
+//     name:"tiya",
+//     age:10,
+//     place:"koratty"
+// }
+// console.log(student);
+// console.log(student.name);
+// var car={
+//     model:"BMW",
+//     color:"green",
+//     price:75000000
+// }
+// console.log(car);
+
+
+// var person =[
+//     {
+//     name:"tiya",
+//     age:10,
+//     place:"koratty"
+//     },
+//     {
+//         name:"ajas",
+//         age:20,
+//         place:"aluva"
+//     }
+// ]
+// console.log (person[1].name)
+
+
+
+// const addition =(a,b)=>{
+//     var c = a+b;
+
+//     console.log(c)
+
+// }
+// addition(1,2)
+
+
+// 
+// 
+
+
+// var fruit =["apply","orange","mango","grapes"]
+// for(let i=0,i<fruit.length;i++){
+//     console.log(fruit[i]);
+
+// }
